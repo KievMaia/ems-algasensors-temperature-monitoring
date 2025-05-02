413cecf1ae51bc36efc9dfe77a09268867103c8e
p0660061 <kiev.maia@portoseguro.com.br>
2025-05-02 18:24:21 -0300

    Ajusta os nomes dos pacotes.

--- src/main/java/br/com/kiev/temperature/monitoring/TemperatureMonitoringApplication.java
+++ src/main/java/br/com/kiev/temperature/monitoring/TemperatureMonitoringApplication.java
@@ -1,4 +1,4 @@
-package br.com.kiev.temperaturemonitoring;
+package br.com.kiev.temperature.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/kiev/temperature/monitoring/TemperatureMonitoringApplicationTests.java
+++ src/test/java/br/com/kiev/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.kiev.temperaturemonitoring;
+package br.com.kiev.temperature.monitoring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
