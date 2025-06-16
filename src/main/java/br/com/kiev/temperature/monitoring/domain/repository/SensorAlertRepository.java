c0320b6d20278c4d94505313b7c809eb57b6bcc5
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-16 13:35:47 -0300

    Retira import sem uso.

--- src/main/java/br/com/kiev/temperature/monitoring/domain/repository/SensorAlertRepository.java
+++ src/main/java/br/com/kiev/temperature/monitoring/domain/repository/SensorAlertRepository.java
@@ -2,7 +2,6 @@ package br.com.kiev.temperature.monitoring.domain.repository;
 
 import br.com.kiev.temperature.monitoring.domain.model.SensorAlert;
 import br.com.kiev.temperature.monitoring.domain.model.SensorId;
-import br.com.kiev.temperature.monitoring.domain.model.SensorMonitoring;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
